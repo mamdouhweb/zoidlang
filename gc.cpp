96a4550b1569a1f7b31ba3fccdbbb96183dc3784
Radek Slupik <daknok@me.com>
2013-01-08 13:43:20 +0100

    Use std::unique_ptr in the GC
    This makes the code much cleaner and safer, and much more idiomatic
    C++.

--- gc.cpp
+++ gc.cpp
@@ -1,9 +1,5 @@
 #include "gc.hpp"
 
-zlang::garbage_collector::~garbage_collector() {
-    for (auto* obj : objects) delete obj;
-}
-
 void zlang::garbage_collector::add_root(gc_object* obj) {
     roots.insert(obj);
 }
@@ -13,13 +9,12 @@ void zlang::garbage_collector::remove_root(gc_object* obj) {
 }
 
 void zlang::garbage_collector::operator()() {
-    for (auto* obj : roots) obj->mark();
-    for (auto* obj : objects) {
+    for (auto& obj : roots) obj->mark();
+    for (auto& obj : objects) {
         if (!obj->marked) {
-            delete obj;
             objects.erase(obj);
         }
     }
-    for (auto* obj : roots) obj->unmark();
+    for (auto& obj : roots) obj->unmark();
 }
 
--- gc.hpp
+++ gc.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "object.hpp"
+#include <memory>
 #include <set>
 #include <type_traits>
 #include <utility>
@@ -8,24 +9,12 @@
 namespace zlang {
     class garbage_collector {
     public:
-        garbage_collector() = default;
-        garbage_collector(garbage_collector const&) = delete;
-        garbage_collector(garbage_collector&&) = default;
-        garbage_collector& operator=(garbage_collector const&) = delete;
-        garbage_collector& operator=(garbage_collector&&) = default;
-        ~garbage_collector();
-
         template<class T, class... Args>
         typename std::enable_if<std::is_base_of<gc_object, T>::value, T*>::type
         alloc(Args&&... args) {
-            auto* obj = new T(std::forward<Args>(args)...);
-            try {
-                objects.insert(obj);
-            } catch (...) {
-                delete obj;
-                throw;
-            }
-            return obj;
+            auto pair = objects.insert(
+                std::unique_ptr<T>{new T(std::forward<Args>(args)...)});
+            return static_cast<T*>(pair.first->get());
         }
 
         void add_root(gc_object* obj);
@@ -34,7 +23,7 @@ namespace zlang {
         void operator()();
 
     private:
-        std::set<gc_object*> objects;
+        std::set<std::unique_ptr<gc_object>> objects;
         std::set<gc_object*> roots;
     };
 }
